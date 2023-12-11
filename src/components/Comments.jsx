5b4597ec98e94db2d47dce3e7d3ced8dc3f7b850
Federico Arias <fede.ariass33@gmail.com>
2023-12-11 14:17:35 -0300

    adding stars-icons

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,12 +1,23 @@
 import comillas from '../assets/icons/icon-coments.png'
 import user from '../assets/icons/icon-user.png'
+import star from '../assets/icons/icon-star.png'
 import './Comments.css'
+
 export function Comments ({name, comment}) {
 
   return(
     <div className="Comments">
       <div className="Comments-item">
-        <img src={comillas} alt="" className='icon-comillas'/>
+        <div className="stars-com">
+          <img src={comillas} alt="" className='icon-comillas'/>
+          <div className="stars-container">
+            <img src={star} alt="" className='star'/>
+            <img src={star} alt="" className='star'/>
+            <img src={star} alt="" className='star'/>
+            <img src={star} alt="" className='star'/>
+            <img src={star} alt="" className='star'/>
+          </div>
+        </div>
         <p className='user-comment'>{comment}</p>
         <div className="user-container">
           <img src={user} alt="" className='icon-user'/>
